10d078938379f84a2b8baa9834910fe89b3ba129
Bekzodjon <bekshoxantirak@gmail.com>
2021-10-11 10:46:51 +0500

    Aylana va doira
    aylana va doiraga bog'liq  dasturlar

---
+++ DOIRA yuzini hisoblash.py
@@ -0,0 +1,11 @@
+from math import pi
+print("CREATED BY BEKZODJON_PYTHON_MASTER")
+savol =  "Doira radiusini kiritng [SMLARDA]. Chiqish uchun [exit] deb yozing =  "
+daraja = ''
+while daraja != 'exit':
+    daraja = input(savol)
+    if daraja != 'exit':
+        print("Doira yuzi = ",float(daraja)**2*pi)
+   
+print('Dastur tugadi ')
+print("Biz bilan Qolimg! BEKZODJON_PYTHON_MASTER")
