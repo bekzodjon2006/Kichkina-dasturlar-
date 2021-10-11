6ae10396e9ea7715e561a9d365673fcf5f94acd9
Bekzodjon <bekshoxantirak@gmail.com>
2021-10-11 10:45:15 +0500

    Kvadratga oshiruvchi dastur
    Sonlardi kvadratga oshiradi Marxamat foydalaning!!!

---
+++ kvadratga oshiruvchi dastur.py
@@ -0,0 +1,13 @@
+print("CREATED BEKZODJON_PYTHON_MASTER")
+
+savol =  "Biror son yozing chiqish uchun [exit] deb yozing =>"
+daraja = ''
+while daraja != 'exit':
+    daraja = input(savol)
+    if daraja != 'exit':
+        print("Sonning darajasi = ",float(daraja) ** 2)
+   
+print('Dastur tugadi ')
+print("Biz bilan Qolimg! BEKZODJON_PYTHON_MASTER")
+
+    
